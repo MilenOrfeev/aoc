ca30098764443b9beb9fe91ae3f5e274dcf17a30
MilenOrfeev <milen.orfeev@abv.bg>
2019-12-07 12:24:29 +0000

    Restructure 4.1

--- task4_1.py
+++ task4_1.py
@@ -1,3 +1,4 @@
+
 def never_decreases(num_string):
     for index in range(0, len(num_string) - 1):
         left = int(num_string[index])
@@ -23,11 +24,10 @@ def solve():
     count = 0
     for number in range(min_value, max_value):
         number_string = str(number)
-        if never_decreases(number_string) \
-                and has_adjacent_pair(number_string):
+        if never_decreases(number_string) and has_adjacent_pair(number_string):
 
             count += 1
 
-    print(count)
+    print("There are {0} passwords between {1} and {2}".format(count, min_value, max_value))
 
 
