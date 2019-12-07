6a10c9ec29ac439c49e8c704275deadd3ee2f01a
MilenOrfeev <milen.orfeev@abv.bg>
2019-12-07 11:06:37 +0000

    Solve Task 3 Part 1

--- task3_1.py
+++ task3_1.py
@@ -1,8 +1,58 @@
-from aoc_utils import *
+from aoc_utils import load_lines
+
+
+def next_location(direction, current):
+    if direction == 'R':
+        return current[0], current[1] + 1
+    elif direction == 'D':
+        return current[0] - 1, current[1]
+    elif direction == 'L':
+        return current[0], current[1] - 1
+    elif direction == 'U':
+        return current[0] + 1, current[1]
+    else:
+        raise ValueError("Invalid Input")
+
+
+def populate_locations(moves):
+    locations = set()
+    current = (0, 0)
+
+    for move in moves:
+        direction = move[0]
+        repetitions = int(move[1:])
+        for count in range(0, repetitions):
+            current = next_location(direction, current)
+            locations.add(current)
+
+    return locations, current
+
+
+def closest_intersection(first_locations, second_moves):
+    centre = (0, 0)
+    current = centre
+    smallest_distance = -1
+
+    for move in second_moves:
+        direction = move[0]
+        repetitions = int(move[1:])
+        for count in range(0, repetitions):
+            current = next_location(direction, current)
+            if current in first_locations:
+                current_distance = manhattan_distance(current, centre)
+                if current_distance < smallest_distance or smallest_distance == -1:
+                    smallest_distance = current_distance
+
+    return smallest_distance
+
+
+def manhattan_distance(first, second):
+    return abs(first[0] - second[0]) + abs(first[1] - second[1])
 
 
 def solve():
-    pass
+    wires = load_lines("input3.txt")
+    locations, centre = populate_locations(wires[0].strip().split(','))
+    closest = closest_intersection(locations, wires[1].strip().split(','))
 
-
-
+    print("Distance from centre to closest intersection is {0}".format(closest))
