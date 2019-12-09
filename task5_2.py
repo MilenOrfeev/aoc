a0f986c73da4332d5b1845b1a9577b981d0d3c21
MilenOrfeev <milen.orfeev@abv.bg>
2019-12-09 22:05:24 +0000

    Complete Task 5 Part 2

--- task5_2.py
+++ task5_2.py
@@ -1,7 +1,7 @@
 from aoc_utils import load_array
 
 
-def do_operation(instructions, action, par_one, par_two, result_index, input_val):
+def do_operation(instructions, action, par_one, par_two, result_index, input_val, index):
     if action is '1':
         instructions[result_index] = par_one + par_two
     elif action is '2':
@@ -10,8 +10,26 @@ def do_operation(instructions, action, par_one, par_two, result_index, input_val
         instructions[par_one] = input_val
     elif action is '4':
         print(par_one)
+    elif action is '5':
+        if par_one is not 0:
+            return par_two
+    elif action is '6':
+        if par_one is 0:
+            return par_two
+    elif action is '7':
+        if par_one < par_two:
+            instructions[result_index] = 1
+        else:
+            instructions[result_index] = 0
+    elif action is '8':
+        if par_one == par_two:
+            instructions[result_index] = 1
+        else:
+            instructions[result_index] = 0
     else:
         raise ValueError("Invalid action {0}".format(action))
+
+    return index + 1
 
 
 def do_instructions(instructions, input_val):
@@ -34,23 +52,26 @@ def do_instructions(instructions, input_val):
         else:
             par_one = instructions[instructions[index]]
 
-        if action in ('1', '2'):
+        if action not in ('3', '4'):
             index += 1
             if len(command) > 3 and command[-4] == '1':
                 par_two = instructions[index]
             else:
                 par_two = instructions[instructions[index]]
-            index += 1
-            result_index = instructions[index]
+
+            if action not in ('5', '6'):
+                index += 1
+                result_index = instructions[index]
+            else:
+                result_index = -1
         else:
             par_two = -1
             result_index = -1
 
-        do_operation(instructions, action, par_one, par_two, result_index, input_val)
-        index += 1
+        index = do_operation(instructions, action, par_one, par_two, result_index, input_val, index)
 
 
 def solve():
     program = load_array("input5.txt")
-    system_id = 1
+    system_id = 5
     do_instructions(program, system_id)
