0bdad2618988182a88980955d0e96a76b221ba49
MilenOrfeev <milen.orfeev@abv.bg>
2019-12-20 23:47:19 +0000

    Reformat task 10 part 1

--- task10_1.py
+++ task10_1.py
@@ -3,34 +3,42 @@ import math
 from aoc_utils import load_lines
 
 
-class Point(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self) -> str:
-        return self.x + " " + self.y
-
-
 def create_graph(lines):
     asteroids = []
     for row_index in range(0, len(lines)):
         for column_index in range(0, len(lines[0])):
             if lines[row_index][column_index] == '#':
-                asteroids.append(Point(row_index, column_index))
+                asteroids.append((row_index, column_index))
 
     return asteroids
 
 
-def solve():
-    asteroids = create_graph(load_lines("input10.txt"))
-    lengths = []
+def get_visible(station_index, asteroids):
+    station = asteroids[station_index]
+
+    asteroids.pop(station_index)
+    length = len(set(math.atan2(asteroid[1] - station[1], asteroid[0] - station[0]) for asteroid in asteroids))
+    asteroids.insert(station_index, station)
+
+    return length
+
+
+def find_best_station(asteroids):
+    max_visible = 0
+    best_station = (0, 0)
     for station_index in range(0, len(asteroids)):
         station = asteroids[station_index]
-        asteroids.pop(station_index)
+        visible = get_visible(station_index, asteroids)
+        if visible > max_visible:
+            max_visible = visible
+            best_station = station
 
-        length = len(set(math.atan2(asteroid.y - station.y, asteroid.x - station.x) for asteroid in asteroids))
-        lengths.append(length)
-        asteroids.insert(station_index, station)
+    formatted = (best_station[1], best_station[0])
+    return max_visible, formatted
 
-    print("The max asteroids that can be seen is {}".format(max(lengths)))
+
+def solve():
+    asteroids = create_graph(load_lines("input10.txt"))
+    max_visible, best_station = find_best_station(asteroids)
+
+    print("The best station is at {} and you can see {} asteroids".format(best_station, max_visible))
